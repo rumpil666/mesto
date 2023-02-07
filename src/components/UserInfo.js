362235e616a1fad0fe115e0e4a9ea811943a88d3
rumpil666 <113824717+rumpil666@users.noreply.github.com>
2023-02-07 18:01:26 +0400

    a

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,15 @@
 export default class UserInfo {
-  constructor({username, job}) {
+  constructor({username, job, avatar}) {
     this._username = document.querySelector(username);
     this._job = document.querySelector(job);
+    this._avatar = document.querySelector(avatar);
   }
 
   getUserInfo() {
     const userInfo = {
       username: this._username.textContent,
       job: this._job.textContent,
+      avatar: this._avatar.src,
     }
 
     return userInfo;
@@ -16,5 +18,6 @@ export default class UserInfo {
   setUserInfo(data) {
     this._username.textContent = data.name;
     this._job.textContent = data.job;
+    this._avatar.src = data.avatar;
   }
 }
